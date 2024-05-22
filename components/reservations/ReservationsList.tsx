9b46d2c5c98d9763ca2123e4024f61243df8df6f
Stephane Mfuni <stephuni35@gmail.com>
2024-05-22 13:37:49 +0300

    fix reservation item list

--- components/reservations/ReservationsList.tsx
+++ components/reservations/ReservationsList.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
 import { StatusRenderComponent } from "../dashboard/ActivitiesComponent";
+import { useRouter } from "next/navigation";
 
 type Props = {
   reservation: ReservationsType;
@@ -14,14 +15,18 @@ const ReservationsList = ({ reservation }: Props) => {
 
   const { data: session } = useSession();
   const user = session?.user;
+  const router = useRouter();
 
   if (!user) {
     return null;
   }
 
   return (
-    <Link
-      href={`/reservations/${reservation.id}`}
+    <div
+      onClick={() => {
+        router.push(`/reservations/${reservation.id}`);
+      }}
+      //   href={`/reservations/${reservation.id}`}
       className="w-full p-2 border box-border shadow cursor-pointer 
     transition-all duration-300 ease-in-out"
     >
@@ -46,15 +51,39 @@ const ReservationsList = ({ reservation }: Props) => {
         {/* infos */}
 
         <div className="box-border text-xs capitalize flex flex-col items-start flex-1 gap-[0.2rem]">
-          <h2 className="font-bold line-clamp-1 flex-1 text-sm">
-            {jobber.user.name}
-          </h2>
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push(`/services/${jobber.user.id}`);
+            }}
+          >
+            <h2
+              className="font-bold line-clamp-1 flex-1 text-sm hover:underline
+             transition-all duration-300 ease-in-out"
+            >
+              {jobber.user.name}
+            </h2>
+          </div>
+          {/* <div
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push(`/services?job=${jobber.job}`);
+            }}
+          > */}
           <p
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push(`/services?job=${jobber.job}`);
+            }}
             className=" bg-gradient-to-r from-green-700 
-            via-green-600 to-green-400 inline-block text-transparent bg-clip-text line-clamp-1"
+            via-green-600 to-green-400 text-transparent hover:underline 
+            hover:opacity-80
+             bg-clip-text line-clamp-1 transition-all duration-300 ease-in-out"
           >
             {jobber.job}
           </p>
+          {/* </div> */}
+
           <p className="line-clamp-1 opacity-80">+243-{jobber.user.tel}</p>
         </div>
       </div>
@@ -79,7 +108,7 @@ const ReservationsList = ({ reservation }: Props) => {
         {/* status */}
         <StatusRenderComponent status={reservation.status} />
       </div>
-    </Link>
+    </div>
   );
 };
 
