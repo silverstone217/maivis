6a61a67d940f5a97bc0bd988fc29d877f927f52a
Stephane Mfuni <stephuni35@gmail.com>
2024-05-22 13:40:21 +0300

    fix loading.tsx padding

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,7 @@ const loading = () => {
   return (
     <main className="min-h-full 2xl:max-w-7xl mx-auto w-full">
       <div className="mt-10" />
-      <div className="flex min-h-96 flex-col p-24 items-center w-full justify-center gap-y-6">
+      <div className="flex min-h-96 flex-col p-14 lg:p-24 items-center w-full justify-center gap-y-6">
         <LogoComponent />
         <p className="">
           Chez <strong className="text-primary">Maivis</strong> Trouves
